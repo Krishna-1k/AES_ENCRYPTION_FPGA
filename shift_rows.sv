38e38ed306bf0e68a9b1062d57d87c918de5b776
Krishna-1k <krishna.v.karthik@gmail.com>
2025-03-17 21:43:38 -0700

    Update shift_rows.sv

--- shift_rows.sv
+++ shift_rows.sv
@@ -15,48 +15,26 @@ module shift_rows
 			  data_shift_temp_out <= 0;
 			  assert_o_en <= 0;
 	    end else begin
-	        //first row 
-	      /*  data_shift_temp_out[7:0] <= data_in[7:0];
-           data_shift_temp_out[39:32] <= data_in[39:32];
-			  data_shift_temp_out[71:64] <= data_in[71:64];
-           data_shift_temp_out[103:96] <= data_in[103:96];
-			  
-			  
-			  //second row
-	        data_shift_temp_out[15:8] <= data_in[47-:8];
-           data_shift_temp_out[47:40] <= data_in[79-:8];
-			  data_shift_temp_out[79:72] <= data_in[111-:8];
-           data_shift_temp_out[111:104] <= data_in[15-:8];
 
-			  
-			  //third row
-	        data_shift_temp_out[23:16] <= data_in[87-:8];
-           data_shift_temp_out[55:48] <= data_in[119-:8];
-			  data_shift_temp_out[87:80] <= data_in[23-:8];
-           data_shift_temp_out[119:112] <= data_in[55-:8];
-			  
-			  //fourth row
-	        data_shift_temp_out[31:24] <= data_in[127-:8];
-           data_shift_temp_out[63:56] <= data_in[31-:8];
-			  data_shift_temp_out[95:88] <= data_in[63-:8];
-           data_shift_temp_out[127:120] <= data_in[95-:8];
-			  */
-
+			  //first row
 			  data_shift_temp_out[24+:8] <= data_in[24+:8];
 			  data_shift_temp_out[56+:8] <= data_in[56+:8];
 			  data_shift_temp_out[88+:8] <= data_in[88+:8];
 			  data_shift_temp_out[120+:8] <= data_in[120+:8];
 		 
+		     //second row
 			  data_shift_temp_out[16+:8] <= data_in[112+:8];
 			  data_shift_temp_out[48+:8] <= data_in[16+:8];
 			  data_shift_temp_out[80+:8] <= data_in[48+:8];
 			  data_shift_temp_out[112+:8] <= data_in[80+:8]; 
 			  
+			  //third row
 			  data_shift_temp_out[8+:8] <= data_in[72+:8];
 			  data_shift_temp_out[40+:8] <= data_in[104+:8];
 			  data_shift_temp_out[72+:8] <= data_in[8+:8];
 			  data_shift_temp_out[104+:8] <= data_in[40+:8]; 
 		
+		     //fourth row
 			  data_shift_temp_out[0+:8] <= data_in[32+:8];
 			  data_shift_temp_out[32+:8] <= data_in[64+:8];
 			  data_shift_temp_out[64+:8] <= data_in[96+:8];
@@ -90,23 +68,3 @@ endmodule
 */ 
 
  
-
-
-			  /* Where first four bytes fill first row(not correct as not enough scrambling)
-			  
-			  
-			  	        //first row 
-	        data_shift_temp_out[31:0] <= data_in[31:0];
-			  
-			  //second row
-			  data_shift_temp_out[55:32] <= data_in[63:40]; 
-  			  data_shift_temp_out[63:56] <= data_in[39:32];
-
-			  
-			  //third row
-  			  data_shift_temp_out[95:80] <= data_in[79:64];
-			  data_shift_temp_out[79:64] <= data_in[95:80];
-			  
-			  //fourth row
-			  data_shift_temp_out[103:96] <= data_in[119:96];
-			  data_shift_temp_out[127:104] <= data_in[127:120]; */
